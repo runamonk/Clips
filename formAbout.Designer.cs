822a030e736eaedc7d3473c683a7538b2d11d647
runamonk <runamonk@gmail.com>
2020-12-28 11:48:23 +0000

    Added formAbout.

---
+++ formAbout.Designer.cs
@@ -0,0 +1,130 @@
+﻿namespace Clips
+{
+    partial class formAbout
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(formAbout));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.lblVersion = new System.Windows.Forms.Label();
+            this.lblName = new System.Windows.Forms.Label();
+            this.linkEmail = new System.Windows.Forms.LinkLabel();
+            this.lblDesc = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Controls.Add(this.lblDesc);
+            this.panel1.Controls.Add(this.linkEmail);
+            this.panel1.Controls.Add(this.lblVersion);
+            this.panel1.Controls.Add(this.lblName);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(275, 180);
+            this.panel1.TabIndex = 0;
+            // 
+            // lblVersion
+            // 
+            this.lblVersion.Dock = System.Windows.Forms.DockStyle.Top;
+            this.lblVersion.ForeColor = System.Drawing.Color.White;
+            this.lblVersion.Location = new System.Drawing.Point(0, 22);
+            this.lblVersion.Name = "lblVersion";
+            this.lblVersion.Size = new System.Drawing.Size(273, 23);
+            this.lblVersion.TabIndex = 1;
+            this.lblVersion.Text = "[VERSION]";
+            this.lblVersion.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblName
+            // 
+            this.lblName.Dock = System.Windows.Forms.DockStyle.Top;
+            this.lblName.ForeColor = System.Drawing.Color.White;
+            this.lblName.Location = new System.Drawing.Point(0, 0);
+            this.lblName.Name = "lblName";
+            this.lblName.Size = new System.Drawing.Size(273, 22);
+            this.lblName.TabIndex = 0;
+            this.lblName.Text = "[NAME]";
+            this.lblName.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // linkEmail
+            // 
+            this.linkEmail.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.linkEmail.LinkColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.linkEmail.Location = new System.Drawing.Point(0, 147);
+            this.linkEmail.Name = "linkEmail";
+            this.linkEmail.Size = new System.Drawing.Size(273, 31);
+            this.linkEmail.TabIndex = 3;
+            this.linkEmail.TabStop = true;
+            this.linkEmail.Text = "runamonk@onlyzuul.org";
+            this.linkEmail.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.linkEmail.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel1_LinkClicked);
+            // 
+            // lblDesc
+            // 
+            this.lblDesc.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblDesc.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.lblDesc.ForeColor = System.Drawing.Color.White;
+            this.lblDesc.Location = new System.Drawing.Point(0, 45);
+            this.lblDesc.Name = "lblDesc";
+            this.lblDesc.Size = new System.Drawing.Size(273, 102);
+            this.lblDesc.TabIndex = 4;
+            this.lblDesc.Text = resources.GetString("lblDesc.Text");
+            this.lblDesc.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // formAbout
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.ClientSize = new System.Drawing.Size(275, 180);
+            this.ControlBox = false;
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "formAbout";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "                                                         ";
+            this.TopMost = true;
+            this.Deactivate += new System.EventHandler(this.formAbout_Deactivate);
+            this.Load += new System.EventHandler(this.formAbout_Load);
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label lblVersion;
+        private System.Windows.Forms.Label lblName;
+        private System.Windows.Forms.LinkLabel linkEmail;
+        private System.Windows.Forms.Label lblDesc;
+    }
+}
---
+++ formAbout.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Utility;
+
+namespace Clips
+{
+    public partial class formAbout : Form
+    {
+        public formAbout()
+        {
+            InitializeComponent();
+        }
+
+        private void formAbout_Load(object sender, EventArgs e)
+        {
+            lblName.Text = Funcs.GetName();
+            lblVersion.Text = "Version: " + Funcs.GetVersion();
+
+            // Center over parent since CenterParent only works if the form is shown as a dialog.
+            if (Owner != null)
+                Location = new Point(Owner.Location.X + Owner.Width / 2 - Width / 2,
+                    Owner.Location.Y + Owner.Height / 2 - Height / 2);
+        }
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            System.Diagnostics.Process.Start("mailto:" + linkEmail.Text);
+        }
+
+        private void formAbout_Deactivate(object sender, EventArgs e)
+        {
+            Close();
+        }
+    }
+}
